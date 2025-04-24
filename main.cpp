58ee762f0e6bc95707c280c629aeba9fde5d48ec
Muhammad Waleed <waleedshahbaz01@gmail.com>
2025-04-24 12:52:00 +0500

    Login feature done(i think so) but not tested

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include "Student.h"
+#include "Teacher.h"
+#include "UserManagement.h"
+#include "Validations.h"
+
 using namespace std;
 
-int main() {
-	cout << "SMS";
-	return 0;
-    cout<<"Hi";
+int main()
+{
+    int choice;
+    cout << "Welcome to University Portal\n";
+    cout << "1. Admin Login\n";
+    cout << "2. Student/Teacher Login\n";
+    cout << "Enter choice: ";
+    cin >> choice;
+
+    if (choice == 1)
+    {
+        runAdminPanel();
+    }
+    else if (choice == 2) 
+    {
+        runUserLogin();
+    }
+    else 
+    {
+        cout << "Invalid option.\n";
+    }
+
+    return 0;
 }
